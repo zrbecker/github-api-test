4083548320a6cb9da475b3c583c415af607dbb81
Zachary Becker <zrbecker@gmail.com>
2017-10-21 01:28:52 -0700

    github authorization flow

---
+++ index.js
@@ -0,0 +1,17 @@
+const config = require('config')
+const github_auth = require('./lib/github_auth')
+const winston = require('winston')
+
+const GithubAuthHandler = github_auth.GithubAuthHandler
+
+;(async () => {
+  try {
+    const {client_id, redirect_uri} = config.get('github_api')
+    const authHandler = new GithubAuthHandler(client_id, redirect_uri)
+    const autherizationCode = await authHandler.authorize()
+    winston.debug(`Access token "${autherizationCode}"`)
+    await authHandler.close()
+  } catch (e) {
+    console.error(e)
+  }
+})()
